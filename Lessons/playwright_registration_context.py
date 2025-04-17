55b000cc17dcd7dc9c3cfe07928e9c74deaeb9c0
Alexander Kadenev <crohntv@gmail.com>
2025-04-17 15:03:58 +0700

    Auth context lessons + .gitignore

---
+++ Lessons/playwright_registration_context.py
@@ -0,0 +1,10 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json") # Указываем файл с сохраненным состоянием
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+
+    page.wait_for_timeout(5000)
