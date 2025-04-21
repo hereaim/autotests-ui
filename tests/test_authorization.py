9a8702f1a450bf6d6e1e30167ce2cc99c41523f0
Alexander Kadenev <crohntv@gmail.com>
2025-04-21 17:18:00 +0700

    PageObject

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,6 +1,8 @@
 from playwright.sync_api import Page, expect
 import pytest
 
+from pages.login_page import LoginPage
+
 auth_data = [
     ("user.name@gmail.com", "password"),
     ("user.name@gmail.com", "  "),
@@ -10,18 +12,10 @@ auth_data = [
 @pytest.mark.regression
 @pytest.mark.authorization
 @pytest.mark.parametrize("email, password", auth_data)
-def test_wrong_email_or_password_authorization(email: str, password: str, chromium_page: Page):
-    chromium_page.goto(
+def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str, password: str):
+    login_page.visit(
         "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login"
     )
-    email_input = chromium_page.get_by_test_id("login-form-email-input").locator("input")
-    email_input.fill(email)
-    password_input = chromium_page.get_by_test_id("login-form-password-input").locator("input")
-    password_input.fill(password)
-    login_button = chromium_page.get_by_test_id("login-page-login-button")
-    login_button.click()
-    wrong_email_or_password_alert = chromium_page.get_by_test_id(
-        "login-page-wrong-email-or-password-alert"
-    )
-    expect(wrong_email_or_password_alert).to_be_visible()
-    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
+    login_page.fill_login_form(email=email, password=password)
+    login_page.click_login_button()
+    login_page.check_vivsble_wrong_email_or_password_alert()
