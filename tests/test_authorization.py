2e422582148c0f7c0cf6161527878dd25bb48959
Alexander Kadenev <crohntv@gmail.com>
2025-04-21 13:36:43 +0700

    Параметризация Pytest. Практика работы с pytest параметризацией

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,16 +1,23 @@
 from playwright.sync_api import Page, expect
 import pytest
 
+auth_data = [
+    ("user.name@gmail.com", "password"),
+    ("user.name@gmail.com", "  "),
+    ("  ", "password")
+]
+
 @pytest.mark.regression
 @pytest.mark.authorization
-def test_wrong_email_or_password_authorization(chromium_page: Page):
+@pytest.mark.parametrize("email, password", auth_data)
+def test_wrong_email_or_password_authorization(email: str, password: str, chromium_page: Page):
     chromium_page.goto(
         "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login"
     )
     email_input = chromium_page.get_by_test_id("login-form-email-input").locator("input")
-    email_input.fill("user.name@gmail.com")
+    email_input.fill(email)
     password_input = chromium_page.get_by_test_id("login-form-password-input").locator("input")
-    password_input.fill("password")
+    password_input.fill(password)
     login_button = chromium_page.get_by_test_id("login-page-login-button")
     login_button.click()
     wrong_email_or_password_alert = chromium_page.get_by_test_id(
