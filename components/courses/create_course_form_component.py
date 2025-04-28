a918b94a545f455d3c331c443be9c941e4087275
Alexander Kadenev <crohntv@gmail.com>
2025-04-28 10:47:05 +0700

    Практические применение PageFactory в автотестах. Практикуемся в применении Page Factory

--- components/courses/create_course_form_component.py
+++ components/courses/create_course_form_component.py
@@ -1,6 +1,8 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
 from components.base_component import BaseComponent
+from elements.input import Input
+from elements.textarea import Textarea
 
 
 class CreateCourseFormComponent(BaseComponent):
@@ -8,24 +10,19 @@ class CreateCourseFormComponent(BaseComponent):
         super().__init__(page)
 
         # Форма создания курса
-        self.title_input = page.get_by_test_id(
-            "create-course-form-title-input"
-        ).locator("input")
-        self.estimated_time_input = page.get_by_test_id(
-            "create-course-form-estimated-time-input"
-        ).locator("input")
-        self.description_textarea = (
-            # При поиске поля описания будет найдено два тега textarea, берем первый из них
-            page.get_by_test_id("create-course-form-description-input")
-            .locator("textarea")
-            .first
+        self.title_input = Input(page, "create-course-form-title-input", "Title")
+        self.estimated_time_input = Input(
+            page, "create-course-form-estimated-time-input", "Estimated time"
         )
-        self.max_score_input = page.get_by_test_id(
-            "create-course-form-max-score-input"
-        ).locator("input")
-        self.min_score_input = page.get_by_test_id(
-            "create-course-form-min-score-input"
-        ).locator("input")
+        self.description_textarea = Textarea(
+            page, "create-course-form-description-input", "Description"
+        )
+        self.max_score_input = Input(
+            page, "create-course-form-max-score-input", "Max score"
+        )
+        self.min_score_input = Input(
+            page, "create-course-form-min-score-input", "Min score"
+        )
 
     def check_visible(
         self,
@@ -35,20 +32,20 @@ class CreateCourseFormComponent(BaseComponent):
         max_score: str,
         min_score: str,
     ):
-        expect(self.title_input).to_be_visible()
-        expect(self.title_input).to_have_value(title)
+        self.title_input.check_visible()
+        self.title_input.check_have_text(value=title)
 
-        expect(self.estimated_time_input).to_be_visible()
-        expect(self.estimated_time_input).to_have_value(estimated_time)
+        self.estimated_time_input.check_visible()
+        self.estimated_time_input.check_have_text(value=estimated_time)
 
-        expect(self.description_textarea).to_be_visible()
-        expect(self.description_textarea).to_have_value(description)
+        self.description_textarea.check_visible()
+        self.description_textarea.check_have_text(value=description)
 
-        expect(self.max_score_input).to_be_visible()
-        expect(self.max_score_input).to_have_value(max_score)
+        self.max_score_input.check_visible()
+        self.max_score_input.check_have_text(value=max_score)
 
-        expect(self.min_score_input).to_be_visible()
-        expect(self.min_score_input).to_have_value(min_score)
+        self.min_score_input.check_visible()
+        self.min_score_input.check_have_text(value=min_score)
 
     def fill(
         self,
@@ -58,13 +55,13 @@ class CreateCourseFormComponent(BaseComponent):
         max_score: str,
         min_score: str,
     ):
-        self.title_input.fill(title)
-        expect(self.title_input).to_have_value(title)
-        self.estimated_time_input.fill(estimated_time)
-        expect(self.estimated_time_input).to_have_value(estimated_time)
-        self.description_textarea.fill(description)
-        expect(self.description_textarea).to_have_value(description)
-        self.max_score_input.fill(max_score)
-        expect(self.max_score_input).to_have_value(max_score)
-        self.min_score_input.fill(min_score)
-        expect(self.min_score_input).to_have_value(min_score)
+        self.title_input.fill(value=title)
+        self.title_input.check_have_text(value=title)
+        self.estimated_time_input.fill(value=estimated_time)
+        self.estimated_time_input.check_have_text(value=estimated_time)
+        self.description_textarea.fill(value=description)
+        self.description_textarea.check_have_text(value=description)
+        self.max_score_input.fill(value=max_score)
+        self.max_score_input.check_have_text(value=max_score)
+        self.min_score_input.fill(value=min_score)
+        self.min_score_input.check_have_text(value=min_score)
--- components/courses/create_course_toolbar_view_component.py
+++ components/courses/create_course_toolbar_view_component.py
@@ -1,27 +1,29 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
 from components.base_component import BaseComponent
+from elements.text import Text
+from elements.button import Button
 
 
 class CreateCourseToolbarViewComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
         # Заголовок и кнопка создания курса
-        self.title = page.get_by_test_id(
-            "create-course-toolbar-title-text"
+        self.title = Text(page,
+            "create-course-toolbar-title-text", 'Title'
         )
-        self.create_button = page.get_by_test_id(
-            "create-course-toolbar-create-course-button"
+        self.create_button = Button(page,
+            "create-course-toolbar-create-course-button", 'Create'
         )
 
     def check_visible(self, is_create_course_disabled=True):
-        expect(self.title).to_be_visible()
-        expect(self.title).to_have_text("Create course")
-        expect(self.create_button).to_be_visible()
+        self.title.check_visible()
+        self.title.check_have_text(text="Create course")
+        self.create_button.check_visible()
         if is_create_course_disabled:
-            expect(self.create_button).to_be_disabled()
+            self.create_button.check_disabled()
         if not is_create_course_disabled:
-            expect(self.create_button).to_be_enabled()
+            self.create_button.check_enabled()
 
     def click_create_course_button(self):
         self.create_button.click()
