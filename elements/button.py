a26d23bbad7932271d4635d56cd95d3653b51035
Alexander Kadenev <crohntv@gmail.com>
2025-05-14 10:59:00 +0700

    Практикуемся в интеграции покрытия в UI тесты. Практическое задание: Работа с инструментом ui-coverage-tool

--- elements/button.py
+++ elements/button.py
@@ -1,5 +1,6 @@
 import allure
 from playwright.sync_api import expect
+from ui_coverage_tool import ActionType
 
 from elements.base_element import BaseElement
 from tools.logger import get_logger
@@ -19,6 +20,7 @@ class Button(BaseElement):
             logger.info(step)
             locator = self.get_locator(nth, **kwargs)
             expect(locator).to_be_enabled()
+        self.tracker_coverage(ActionType.ENABLED, nth,  **kwargs)
 
     def check_disabled(self, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name} is disabled"'
@@ -26,3 +28,4 @@ class Button(BaseElement):
             logger.info(step)
             locator = self.get_locator(nth, **kwargs)
             expect(locator).to_be_disabled()
+        self.tracker_coverage(ActionType.DISABLED, nth,  **kwargs)
--- elements/textarea.py
+++ elements/textarea.py
@@ -2,6 +2,7 @@ import allure
 from playwright.sync_api import Locator, expect
 
 from elements.base_element import BaseElement
+from ui_coverage_tool import ActionType
 from tools.logger import get_logger
 
 
@@ -11,17 +12,23 @@ logger = get_logger("TEXTAREA")
 class Textarea(BaseElement):
     @property
     def type_of(self) -> str:
-        return 'textarea'
+        return "textarea"
 
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
-        return super().get_locator(nth, **kwargs).locator('textarea').first
+        return super().get_locator(nth, **kwargs).locator("textarea").first
 
-    def fill(self, value: str, nth: int = 0,  **kwargs):
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
+        return (
+            f"{super().get_locator(nth, **kwargs).locator('textarea').first}//textarea"
+        )
+
+    def fill(self, value: str, nth: int = 0, **kwargs):
         step = f'Fill {self.type_of} "{self.name}" to value "{value}"'
         with allure.step(step):
             logger.info(step)
             locator = self.get_locator(nth, **kwargs)
             locator.fill(value)
+        self.tracker_coverage(ActionType.FILL, nth, **kwargs)
 
     def check_have_value(self, value: str, nth: int = 0, **kwargs):
         step = f'Check {self.type_of} "{self.name}" have value "{value}"'
@@ -29,3 +36,4 @@ class Textarea(BaseElement):
             logger.info(step)
             locator = self.get_locator(nth, **kwargs)
             expect(locator).to_have_value(value)
+        self.tracker_coverage(ActionType.VALUE,  nth, **kwargs)
